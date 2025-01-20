de1b8ac07a236469492f3165e0eaf2d3edb43838
Nahiyan Khan <nahiyan@squareup.com>
2025-01-19 20:41:01 -0500

    changed to the neon forge

--- app/components/sections/about.tsx
+++ app/components/sections/about.tsx
@@ -1,11 +1,36 @@
 "use client";
 
+import { ReactNode } from "react";
+
+const StrikeThrough = ({
+  children,
+  className,
+}: {
+  children?: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <span
+      className={`text-textSubtle relative transition-all [&::after]:content-[''] [&::after]:absolute [&::after]:transition-all [&::after]:left-0 [&::after]:top-[50%] [&::after]:mt-[-1px] [&::after]:h-[2px] [&::after]:w-full [&::after]:bg-bg ${className}`}
+    >
+      {children}
+    </span>
+  );
+};
+
 export const About = () => {
   return (
-    <div className="w-full pt-40 pb-60 text-textStandard">
-      <div className="font-mono">THE FORGE</div>
-      <div className="mb-2 pb-2 border-b">/fôrj/</div>
-      <div className="">place to craft (something) strong, enduring</div>
+    <div className="w-full pt-40 pb-60 text-textStandard ">
+      <div className="font-mono text-lg mb-2">
+        THE <StrikeThrough>NEON</StrikeThrough> FORGE
+      </div>
+      <div className="mb-3 pb-3 border-b">
+        /T͟Hə/ <StrikeThrough>/ˈnēˌän/</StrikeThrough> /fôrj/
+      </div>
+      <div className="">
+        place to craft (something) strong, enduring{" "}
+        <StrikeThrough>with kickass vibes</StrikeThrough>
+      </div>
     </div>
   );
 };
--- app/components/sections/intro.tsx
+++ app/components/sections/intro.tsx
@@ -6,8 +6,6 @@ import { NeonForge } from "../neon-forge";
 export const Intro = () => {
   return (
     <div>
-      {/* <GridLines columns={6} /> */}
-      {/* <div className="absolute h-[10px] w-full top-[50%] bottom-[50%] translate-y-[-5px] animated-border" /> */}
       <div className="flex h-screen items-center text-textStandard relative">
         <NeonForge />
       </div>
